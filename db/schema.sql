6968d2e7f7254c91703743805972243276bfece9
Shivey Singh <shivey_khangura@hotmail.com>
2021-01-14 01:28:48 +0000

    added database

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS traveltracker_db;
+CREATE DATABASE traveltracker_db;
+
+USE traveltracker_db;
+
+CREATE TABLE holidays (
+  id INT NOT NULL AUTO_INCREMENT,
+  cityname VARCHAR(255) NOT NULL,
+  hotel VARCHAR(255) NOT NULL,
+  restaurant VARCHAR(255) NOT NULL,
+  review VARCHAR(255) NOT NULL,
+  PRIMARY KEY(id)
+);
