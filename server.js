5ee0ba0e3958f659fb0098aa4e4f8c0e467348ab
Shivey Singh <shivey_khangura@hotmail.com>
2021-01-14 23:52:38 +0000

    added single quotes

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 // Requiring npm packages
 const express = require('express');
+<<<<<<< Updated upstream
+=======
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+>>>>>>> Stashed changes
 require('dotenv').config();
 
 // Setting up PORT if .env exists or default to 3030
@@ -15,6 +21,31 @@ app.use(express.static('public'));
 // Requiring routes
 require('./routes/html-routes')(app);
 
+<<<<<<< Updated upstream
+=======
+//uese method override for forum PUT and DELETE queries
+app.use(methodOverride('_method'));
+
+//set engine and default for handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}) );
+app.set('view engine', 'handlebars');
+
+//set body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+//serve static content from the public directory
+app.use(express.static(__dirname + '/public'));
+
+//require holiday-controller,js for the routes
+let routes = require('./controllers/holiday_controller.js');
+
+app.use('/', routes);
+app.use('/:id', routes);
+
+>>>>>>> Stashed changes
 app.listen(PORT, function() {
   console.log('Hello and welcome to project 2 express server');
   console.log('I am running on port', PORT);
