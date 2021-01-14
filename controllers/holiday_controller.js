a6efce6cd55b6ae229ab570aa126f171d6d29008
Shivey Singh <shivey_khangura@hotmail.com>
2021-01-14 21:51:50 +0000

    used cloudinary and return variable

---
+++ controllers/holiday_controller.js
@@ -0,0 +1,40 @@
+let express = require('express');
+let holidayORM = require('../models/holiday.js');
+let cloudinary = require ('cloudinary').v2;
+
+let router = express.Router();
+//create routers and export them
+
+//select all holidays from the db and render them
+router.get('/', function(req, res) {
+  holidayORM.selectAll(function(data) {
+    res.render('index', {holiday:data});
+  });
+});
+
+//post a new holiday to the db
+router.post('/', function(req, res) {
+  holidayORM.insertOne(req.body.holiday, function(result) {
+    console.log(result);
+    res.redirect('/');
+  });
+});
+
+//update a holiday's status
+router.put('/:id', function(req,res) {
+  holidayORM.updateOne(req.params.id, function(result) {
+    console.log(result);
+    res.redirect('/');
+  });
+});
+
+//delete a holiday from the database
+router.delete('/:id', function(req,res) {
+  holidayORM.deleteOne(req.params.id, function(result) {
+    console.log(result);
+    res.redirect('/');
+  });
+});
+
+module.exports = router,cloudinary;
+
