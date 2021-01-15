7aa05b7b476ac640a6cb8b77d3d04165df91f4c9
Shivey Singh <shivey_khangura@hotmail.com>
2021-01-15 10:49:05 +0000

    added single quotes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // Requiring npm packages
 const express = require('express');
-const exphbs = require("express-handlebars");
-const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 require('dotenv').config();
 
 // Setting up PORT if .env exists or default to 3030
@@ -19,20 +19,20 @@ app.use(express.static('public'));
 require('./routes/html-routes')(app);
 
 //uese method override for forum PUT and DELETE queries
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'));
 
 //set engine and default for handlebars
-app.engine("handlebars", exphbs({ defaultLayout: "main"}) );
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}) );
+app.set('view engine', 'handlebars');
 
 //set body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 //serve static content from the public directory
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
 //require holiday-controller,js for the routes
 let routes = require("./controllers/holiday_controller.js");
