9be6f98a9cf38271671b743acfe28b7e846d6c79
Shivey Singh <shivey_khangura@hotmail.com>
2021-01-16 13:01:46 +0000

    Update html-routes.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,7 +1,17 @@
+const router = require("../controllers/holiday_controller");
+const holidayORM =require("../models/holiday")
 
-module.exports = function(app) {
+module.exports = function(router) {
 
-  app.get('/', function(req,res) {
-    res.send('<h1>Homepage</h1>');
-  });
-};
+  router.get('/', function(req, res) {
+  holidayORM.selectAll(function(data) {
+  res.render('index', {holiday:data});
+    });
+
+    router.post('/', function(req, res) {
+      holidayORM.insertOne(req.body.holiday, function(result) {
+        console.log(result);
+        res.redirect('submit');
+      });
+    });
+  })};
