18dada08a784dfa3779a0dd356a5c5f77a1bc62f
Shivey Singh <shivey_khangura@hotmail.com>
2021-01-19 19:00:28 +0000

    added recent code

--- public/assets/css/js/submit.js
+++ public/assets/css/js/submit.js
@@ -1,18 +1,16 @@
-const submitButton = ('submitBtn');
+const submitButton = $('#submitButton');
 const photoFile = $('#photoFile');
 let formData;
 submitButton.click((event) => {
   console.log('clicking');
   event.preventDefault();
-  formData = new formData();
-  formData.append('image', photoFile);
-});
-$.post({
-  url: '/submit/review',
-  data: formData,
-  processData: false,
-  contentType: false,
-}).then(() => {
-  console.log('running post request');
-  // console.log(res);
+  $.post({
+    url: '/submit/review',
+    data: formData,
+    processData: false,
+    contentType: false,
+  }).then(() => {
+    console.log('running post request');
+    // console.log(res);
+  });
 });
