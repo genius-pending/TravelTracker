50592dbcebf6cec7a738a769fafb2cd7c8c5d801
Nikstar-1 <rk_787@hotmail.co.uk>
2021-01-19 22:48:17 +0000

    cloudinary widget in progress

--- public/assets/css/js/submit.js
+++ public/assets/css/js/submit.js
@@ -1,4 +1,4 @@
-const submitButton = $('#submitButton');
+/*const submitButton = $('#submitButton');
 //const photoFile = $('#photoFile');
 let formData;
 submitButton.click((event) => {
@@ -14,3 +14,23 @@ submitButton.click((event) => {
     // console.log(res);
   });
 });
+
+$(document).ready(function() {
+  $("input.cloudinary-fileupload[type=file]").cloudinary_fileupload();
+});
+
+const file = result.info.url;
+*/
+var myWidget = cloudinary.createUploadWidget({
+  cloudName: 'CLOUDINARY_CLOUD_NAME', 
+  uploadPreset: 'my_preset'}, (error, result) => { 
+    if (!error && result && result.event === "success") { 
+      console.log('Done! Here is the image info: ', result.info); 
+    }
+  }
+)
+
+document.getElementById("upload_widget").addEventListener("click", function(){
+  console.log("upload clicked")
+      myWidget.open();
+    }, false);
