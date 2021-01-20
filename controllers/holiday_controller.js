25e3eb419d993d6cb41d12b186da235ed2cc43f3
Shivey Singh <shivey_khangura@hotmail.com>
2021-01-20 21:30:45 +0000

    travis pass

--- controllers/holiday_controller.js
+++ controllers/holiday_controller.js
@@ -15,12 +15,12 @@ router.get('/', function(req, res) {
 
 //post a new holiday to the db
 router.post('/', function(req, res) {
-submitReview(req.body, )
+  submitReview(req.body, );
   console.log('JSON recieved by post api endpoint', req.body);
   /*holidayORM.insertOne(req.body.holiday, function(result) {
     console.log(result);*/
-    res.redirect('/');
-    });
+  res.redirect('/');
+});
 
 
 //update a holiday's status
