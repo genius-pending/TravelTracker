d46b9e58f154355dde8ffa18f1c1de47e6290f01
Pawel <paweln993@gmail.com>
2021-01-20 11:16:37 +0000

    js files paths updated, sorted jquery issues

--- public/assets/js/submit.js
+++ public/assets/js/submit.js
@@ -1,16 +1,19 @@
-const submitButton = $('#submitButton');
-//const photoFile = $('#photoFile');
-let formData;
-submitButton.click((event) => {
-  console.log('clicking');
-  event.preventDefault();
-  $.post({
-    url: '/submit/review',
-    data: formData,
-    processData: false,
-    contentType: false,
-  }).then(() => {
-    console.log('running post request');
-    // console.log(res);
+$(document).ready(function () {
+  const submitButton = $('#submit-btn');
+  submitButton.click((event) => {
+    event.preventDefault();
+    // $.post({
+    //   url: '/submit/review',
+    //   data: formData,
+    //   processData: false,
+    //   contentType: false,
+    // }).then(() => {
+    //   console.log('running post request');
+    //   // console.log(res);
+    // }).catch((err) => {
+    //   if (err) {
+    //     console.log('Error on post:', err);
+    //   }
+    // })
   });
 });
