4a8888ece314e9531e9ff50d93e2ad30e654ec8c
Nikstar-1 <rk_787@hotmail.co.uk>
2021-01-20 04:05:16 +0000

    Upload preset must be whitelisted for unsigned uploads - need to fix

--- public/assets/css/js/submit.js
+++ public/assets/css/js/submit.js
@@ -21,30 +21,49 @@ $(document).ready(function() {
 
 const file = result.info.url;
 */
-var cropMyWidget
+var cropMyWidget;
 
-var myCropWidget = cloudinary.createUploadWidget({
-  cloudName: 'demo', uploadPreset: 'preset1', folder: 'widgetUpload', cropping: true}, 
-  (error, result) => { console.log(error, result) })
+var myCropWidget = cloudinary.createUploadWidget(
+  {
+    cloudName: "phase2projectbirmingham",
+    uploadPreset: "ml_default",
+    folder: "widgetUpload",
+    cropping: true,
+  },
+  (error, result) => {
+    console.log(error, result);
+  }
+);
 
-var myWidget = cloudinary.createUploadWidget({
-  cloudName: 'CLOUDINARY_CLOUD_NAME', 
-  uploadPreset: 'my_preset'}, (error, result) => { 
-    if (!error && result && result.event === "success") { 
-      console.log('Done! Here is the image info: ', result.info); 
+var myWidget = cloudinary.createUploadWidget(
+  {
+    cloudName: "phase2projectbirmingham",
+    uploadPreset: "ml_default",
+  },
+  (error, result) => {
+    if (!error && result && result.event === "success") {
+      console.log("Done! Here is the image info: ", result.info);
     }
   }
-)
+);
 
-document.getElementById("upload_widget").addEventListener("click", function(){
-  console.log("upload clicked")
-      myWidget.open();
-    }, false);
+document.getElementById("upload_widget").addEventListener(
+  "click",
+  function () {
+    console.log("upload clicked");
+    myWidget.open();
+  },
+  false
+);
 
-    cloudinary.openUploadWidget({
-      cloudName: "demo", uploadPreset: "preset1",
-      sources: [ 'local', 'url', 'image_search'],
-      googleApiKey: 'AIrFcR8hKiRo',
-      searchBySites: ["all", "cloudinary.com"],
-      searchByRights: true }, (error, result) => { });
-    
+cloudinary.openUploadWidget(
+  {
+    cloudName: "phase2projectbirmingham",
+    uploadPreset: "ml_default",
+    sources: ["local", "url", "image_search"],
+    googleApiKey: "AIrFcR8hKiRo",
+    searchBySites: ["all", "cloudinary.com"],
+    searchByRights: true,
+  },
+  (error, result) => {}
+);
