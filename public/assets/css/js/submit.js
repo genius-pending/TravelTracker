e4009374d75236fbf3a9bdc25e3dc53ed487fbe7
Nikstar-1 <rk_787@hotmail.co.uk>
2021-01-20 16:41:17 +0000

    added widget functionality, working as it should be now

--- public/assets/css/js/submit.js
+++ public/assets/css/js/submit.js
@@ -16,54 +16,55 @@ submitButton.click((event) => {
 });
 
 $(document).ready(function() {
-  $("input.cloudinary-fileupload[type=file]").cloudinary_fileupload();
+  $('input.cloudinary-fileupload[type=file]').cloudinary_fileupload();
 });
 
 const file = result.info.url;
 */
-var cropMyWidget;
+/*var cropMyWidget;
 
 var myCropWidget = cloudinary.createUploadWidget(
   {
-    cloudName: "phase2projectbirmingham",
-    uploadPreset: "ml_default",
-    folder: "widgetUpload",
+    cloudName: 'phase2projectbirmingham',
+    uploadPreset: 'ml_default',
+    folder: 'widgetUpload',
     cropping: true,
   },
   (error, result) => {
     console.log(error, result);
   }
-);
+);*/
 
 var myWidget = cloudinary.createUploadWidget(
   {
-    cloudName: "phase2projectbirmingham",
-    uploadPreset: "ml_default",
+    cloudName: 'phase2projectbirmingham',
+    uploadPreset: 'xuimcgyf',
   },
   (error, result) => {
-    if (!error && result && result.event === "success") {
-      console.log("Done! Here is the image info: ", result.info);
+    if (!error && result && result.event === 'success') {
+      console.log('Done! Here is the image info: ', result.info);
     }
   }
 );
 
-document.getElementById("upload_widget").addEventListener(
-  "click",
+document.getElementById('upload_widget').addEventListener(
+  'click',
   function () {
-    console.log("upload clicked");
+    console.log('upload clicked');
     myWidget.open();
   },
   false
 );
 
-cloudinary.openUploadWidget(
+/*cloudinary.openUploadWidget(
   {
-    cloudName: "phase2projectbirmingham",
-    uploadPreset: "ml_default",
-    sources: ["local", "url", "image_search"],
-    googleApiKey: "AIrFcR8hKiRo",
-    searchBySites: ["all", "cloudinary.com"],
+    cloudName: 'phase2projectbirmingham',
+    uploadPreset: 'ml_default',
+    sources: ['local', 'url', 'image_search'],
+    googleApiKey: 'AIrFcR8hKiRo',
+    searchBySites: ['all', 'cloudinary.com'],
     searchByRights: true,
   },
   (error, result) => {}
 );
+*/
