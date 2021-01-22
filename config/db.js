711e84df4fe89a35905ae1e6eef83c495481b010
Shivey Singh <shivey_khangura@hotmail.com>
2021-01-22 00:04:05 +0000

    pass travis

--- config/db.js
+++ config/db.js
@@ -11,21 +11,21 @@ if (process.env.JAWSDB_URL) {
       idle: 10000,
     },
   });
-  
-} else
 
-module.exports = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    port: process.env.DB_PORT,
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-  }); 
+} else {
+
+  module.exports = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      port: process.env.DB_PORT,
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+      },
+  })};
